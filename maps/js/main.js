a894fc07d8197573c4b5ae27cb1ed0e05e0cdbf9
vimarshdeo <vimarsh.deo@gmail.com>
2015-10-23 16:01:19 -0700

    change main to download sound first

--- maps/js/main.js
+++ maps/js/main.js
@@ -2,7 +2,7 @@
 var FILE_MANAGER = new FileManager();
 
 FILE_MANAGER.queueDownload('img/bg_image.png');
-
+FILE_MANAGER.queueDownload('sound/1.mp3');
 
 FILE_MANAGER.downloadAll(function() {
     Map.init();
