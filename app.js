be4bf8b14aed1d87cab9e26fd1ff2a2a9f3aacfc
nkumar43 <nikhilkumar89@gmail.com>
2015-11-01 05:46:29 -0700

    added room functionality
    Created socket room, adding players to room functionality

--- app.js
+++ app.js
@@ -1,18 +1,108 @@
-var express = require('express'),
-        app = express(),
-        server = require('http').createServer(app),
-        io = require('socket.io').listen(server);
+var express = require('express');
 var path = require('path');
-server.listen(8080);
-users = {};
+var app = express();
+
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-        res.sendFile(__dirname + '/public');
+	res.sendFile(__dirname + '/public');
+});
+
+var server = require('http').createServer(app).listen(process.env.PORT || 8080);
+var io = require('socket.io').listen(server);
+
+
+io.sockets.on('connection', function (socket) {
+	console.log("A new User Connected");
+	socket. emit('connected', { message: "You are connected!", mySocketId:socket.id });
+
+
+	socket.on('createNewGame', function (message) {
+		console.log("New Game Request got");
+		console.log("User who has joined is: ", message.name);
+
+		var thisGameId = ( Math.random() * 100000 ) | 0;
+		socket.emit('newGameCreated', {gameId: thisGameId, mySocketId: this.id, name:message.name});
+		socket.join(thisGameId.toString());
+	});
+
+	socket.on('inviteFriends', function (message) {
+		if ( message.user1 != ''){
+			console.log("Sending Email to user1");
+			// Send Email to User1
+		}
+		if ( message.user2 != ''){
+			console.log("Sending Email to user2");
+			// Send Email to User2
+		}
+		if ( message.user3 != ''){
+			console.log("Sending Email to user3");
+			// Send Email to User3
+		}
+		if ( message.user4 != ''){
+			console.log("Sending Email to user4");
+			// Send Email to User4
+		}
+		if ( message.user5 != ''){
+			console.log("Sending Email to user5");
+			// Send Email to User5
+		}
+	});
+
+	socket.on('joingamelobby', function (message) {
+		console.log('New Join Room Request');
+		console.log(socket.adapter.rooms);
+		var room = socket.adapter.rooms[message.gameId];
+		if( room != undefined )
+		{
+			socket.join((message.gameId).toString());
+			io.sockets.in(message.gameId).emit('playerJoinedRoom', {gameId: message.gameId,
+				name: message.name, socketid: message.socketid});
+			console.log(socket.adapter.rooms);
+		}
+		else
+		{
+			// Send Error Message
+			//socket.emit('error',{message: "This room does not exist."} );
+		}
+	});
+
+	socket.on('initGame', function (message) {
+		console.log ("Game about to begin");
+		console.log("The Game room is:"+ message.gameId + " and " +"The players are: " + message.users);
+		console.log(message.users.length);
+		io.sockets.in(message.gameId).emit('beginGame');
+	});
+
+	socket.on('loaded', function (message) {
+		console.log("About to send init data");
+		io.sockets.in(message.gameId).emit('init', "Alaska" , "Red" , users[0]);
+		io.sockets.in(message.gameId).emit('init', "NorthWestTerritory", "Red" , users[0]);
+		io.sockets.in(message.gameId).emit('init', "WesternAustralia" , "Blue" , users[1]);
+		io.sockets.in(message.gameId).emit('init', "EasternAustralia" , "Blue" , users[1]);
+		io.sockets.in(message.gameId).emit('init', "NorthAfrica" ,  "Green" , users[2]);
+		io.sockets.in(message.gameId).emit('init', "Egypt" , "Green" , users[2]);
+		io.sockets.in(message.gameId).emit('init', "Yakutsk" , "Yellow" , users[3]);
+		io.sockets.in(message.gameId).emit('init', "Kamchatka" ,"Yellow" , users[3]);
+	});
+
+
+
+	socket.on('deploy', function (message) {
+		socket.broadcast.emit('deploy', message.country, message.count);
+	});
+	socket.on('attack', function (message) {
+		socket.broadcast.emit('attack', message.country, message.count, message.color, message.owner);
+	});
 });
 
 
+
+
+users = {};
+
+/*
 io.on('connection', function (socket) {
 socket.on('new user', function(data, callback){
 		if (data in users){
@@ -72,7 +162,7 @@ socket.broadcast.emit('attack', message.country, message.count, message.color, m
 
 
 
-
+*/
 
 
 
